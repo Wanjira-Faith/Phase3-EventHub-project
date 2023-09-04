13cf401a3fb2efa65df730c5923ee16954d248e6
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-04 20:47:25 +0300

    created events table

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,3 +7,13 @@ engine = create_engine('sqlite:///event_hub.db', echo=True)
 
 # Base class for all classes
 Base = declarative_base()
+
+class Event(Base):
+    __tablename__ = 'events'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    date = Column(Date)
+    description = Column(String(255))
+    capacity = Column(Integer)
+    
