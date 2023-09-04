071edfb444c344dd68428fd28e23e1e43c2408e1
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-04 21:02:34 +0300

    added event_speaker association table

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey, Table
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,6 +8,14 @@ engine = create_engine('sqlite:///event_hub.db', echo=True)
 # Base class for all classes
 Base = declarative_base()
 
+# Many-to-Many Relationship Table (Events - Speakers)
+event_speaker_association = Table(
+    'event_speaker_association',
+    Base.metadata,
+    Column('event_id', Integer, ForeignKey('events.id')),
+    Column('speaker_id', Integer, ForeignKey('speakers.id'))
+)
+
 class Event(Base):
     __tablename__ = 'events'
     
@@ -16,6 +24,11 @@ class Event(Base):
     date = Column(Date)
     description = Column(String(255))
     capacity = Column(Integer)
+
+
+    # Define the many-to-many relationship with speakers
+    speakers = relationship('Speaker', secondary=event_speaker_association, back_populates='events')
+
 
 class Participant(Base):
     __tablename__ = 'participants'
@@ -28,6 +41,10 @@ class Speaker(Base):
     
     id = Column(Integer, primary_key=True)
     name = Column(String(100))
+
+    # Define the many-to-many relationship with events
+    events = relationship('Event', secondary=event_speaker_association, back_populates='speakers')
+
 
 class Venue(Base):
     __tablename__ = 'venues'
