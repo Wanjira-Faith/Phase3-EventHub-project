55c7c2abec8057483b10213c4d2559202f2a2cf9
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-05 15:04:06 +0300

    added event_id to participants table

--- lib/models.py
+++ lib/models.py
@@ -52,6 +52,8 @@ class Participant(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(100))  
     
+    event_id = Column(Integer, ForeignKey('events.id'))
+
     # Define the many-to-one relationship with events
     event = relationship('Event', back_populates='participants')
 
