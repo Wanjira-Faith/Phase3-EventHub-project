6371bfe75a2c8e626a104523be6b977210c0e707
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-06 16:09:11 +0300

    created database engine from utils.py

--- lib/cli.py
+++ lib/cli.py
@@ -1,3 +1,6 @@
 import click
 from models import Base, Event, Participant, Speaker, Venue
 from utils import create_session, create_db_engine
+
+engine = create_db_engine()
+Base.metadata.create_all(engine)
