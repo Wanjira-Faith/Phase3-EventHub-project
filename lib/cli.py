a35d4f2b1c667216f6a8a0b6228f4293d8751e41
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-06 16:40:30 +0300

    command to create new event

--- lib/cli.py
+++ lib/cli.py
@@ -9,4 +9,12 @@ Base.metadata.create_all(engine)
 def cli():
     pass
 
+@cli.command()
+@click.option('--name', prompt='Event Name', help='Name of the event')
+@click.option('--date', prompt='Event Date (YYYY-MM-DD)', help='Date of the event')
+@click.option('--description', prompt='Event Description', help='Description of the event')
+@click.option('--capacity', prompt='Event Capacity', type=int, help='Maximum capacity of the event')
+def create_event(name, date, description, capacity):
+    """Create a new event."""
+
    
